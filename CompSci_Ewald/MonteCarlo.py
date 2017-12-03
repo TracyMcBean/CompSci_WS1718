3d19923351d467a9d57c6f5a52d34fa019dc9d13
TracyMcBean <33286976+TracyMcBean@users.noreply.github.com>
2017-12-03 20:48:02 +0100

    Update MonteCarlo.py
    Adding basic description for methods

--- CompSci_Ewald/MonteCarlo.py
+++ CompSci_Ewald/MonteCarlo.py
@@ -1,9 +1,28 @@
 class MonteCarlo():
 
-    def optimize(self, r_init, parameters):
-        "" calculate most likely configuration of r ""
-        pass
+    def optimize(self, r_init, sigma):
+        """
+        Calculates most likely configuration for the next step.
+        
+        Uses generates random vector with normal distribution.
+        
+        Parameters:
+        r_init: initial state
+        sigma: standard deviation for normal distribution
+        
+        Returns r_opt
+        """
+        
+        return self.r_opt
 
-    def simulate(self, r_opt, ):
-        "" Calculate the next step ""
-        pass
+    def simulate(self, r_opt, phi_init ):
+        """ 
+        Calculate the next step
+        
+        Parameters:
+        r_opt: optimal
+        phi_init: initial potential
+        
+        Returns the new initial position and its potential
+        """
+        return r_init, phi_init
