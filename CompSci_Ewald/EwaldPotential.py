2bc8e9423c7e0482fa9465d2ef1bdeb31c7a756a
TracyMcBean <33286976+TracyMcBean@users.noreply.github.com>
2017-12-03 19:07:37 +0100

    Update EwaldPotential.py
    Adding functions

--- CompSci_Ewald/EwaldPotential.py
+++ CompSci_Ewald/EwaldPotential.py
@@ -1,4 +1,13 @@
 from MonteCarlo import optimize, simulate
 
 class EwaldPotential():
-    
+   """
+   Contains the methods to calculate the potential. 
+   Uses the functions optimize and simulate for the time steps.
+   """
+   def longRange():
+      """
+      Calculates long range potential
+      
+      
+      """
