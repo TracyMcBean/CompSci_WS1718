ac79a2dab017791f380db881240f0f8d4a4d0af5
TracyMcBean <33286976+TracyMcBean@users.noreply.github.com>
2017-12-03 20:29:57 +0100

    Update EwaldPotential.py
    Introducing the methods shortRange and longRange.

--- CompSci_Ewald/EwaldPotential.py
+++ CompSci_Ewald/EwaldPotential.py
@@ -5,9 +5,39 @@ class EwaldPotential():
    Contains the methods to calculate the potential. 
    Uses the functions optimize and simulate for the time steps.
    """
+   
+   def __init__():
+      """
+      Automatically combine long and short Range.
+      """
+      Phi = longRange() + shortRange()
+      
    def longRange():
       """
       Calculates long range potential
       
+      Uses error function.
       
+      Parameters: 
+      rho: charge
+      epsilon: constant
+      sigma: constant
+      positions r and r_i
+      
+      Returns: Phi_longRange
+      """
+
+  def shortRange():
+      """
+      Calculates long range potential
+      
+      Uses complementary error function.
+      
+      Parameters: 
+      rho: charge
+      epsilon: constant
+      sigma: constant
+      positions r and r_i
+      
+      Returns: Phi_shortRange
       """
